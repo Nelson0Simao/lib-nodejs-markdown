cbdd9b86f45e863b839386089c93ca59be363ce5
Nelson0Simao <nelsonsimao100@gmail.com>
2022-08-03 17:55:50 +0100

    sec commint

--- http-validacao.js
+++ http-validacao.js
@@ -1,4 +1,21 @@
-function validaURL(arrayLinks){
-    return arrayLinks;
+async function checaStatus(arrayURLs){
+    // promises async await
+    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+        const res = await fetch(url);
+        return res.status();
+    }));
+    return arrayStatus;
+}
+
+function geraArrayDeURL(arrayLinks){
+    Object.values(arrayLinks);
+
+    return arrayLinks.map(objectLinks => Object.values(objectLinks).join())
+}
+
+async function validaURL(arrayLinks){
+    const links = geraArrayDeURL(arrayLinks);
+    const statusLinks = await checaStatus(links);
+    return statusLinks;
 }
 module.exports = validaURL;
